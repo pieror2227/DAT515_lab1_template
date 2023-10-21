f15f585a12e4b3cb9c0f7ea6e4bf48c292a0180f
pieror2227 <118546863+pieror2227@users.noreply.github.com>
2023-10-21 06:43:56 +0200

    3

--- lab1_student_test.py
+++ lab1_student_test.py
@@ -23,135 +23,40 @@ from tramdata import answer_query
 
 outcome = True
 
+answers = {'via Jaegerdorffsplatsen': ['3', '9'],
+           'via Aprilgatan': ['6'], 
+           'via Scandinavium': ['2', '6', '8', '13'], 
+           'via Botaniska Trädgården': ['1', '2', '7', '8', '13'], 
+           'via Chalmers': ['6', '7', '8', '10', '13'], 
+           'between Temperaturgatan and Sahlgrenska Huvudentré': ['6'], 
+           'between Domkyrkan and Scandinavium': ['2', '6'],
+           'between Bellevue and Positivgatan': ['7'], 
+           'between Saltholmen and Medicinaregatan': ['13'], 
+           'between Medicinaregatan and Saltholmen': ['13'], 
+           'time with 3 from Hagakyrkan to Mariaplan': 16, 
+           'time with 5 from Munkebäckstorget to Sankt Sigfrids Plan': 9, 
+           'time with 7 from Allhelgonakyrkan to Komettorget': 6, 
+           'time with 10 from Wavrinskys Plats to Hjalmar Brantingsplatsen': 15, 
+           'time with 13 from Scandinavium to Korsvägen': 1, 
+           'distance from Jaegerdorffsplatsen to Tranered': 3.003, 
+           'distance from Sahlgrenska Huvudentré to Kortedala Torg': 8.781, 
+           'distance from Temperaturgatan to Lackarebäck': 10.092, 
+           'distance from Rymdtorget Spårvagn to Hagakyrkan': 8.841, 
+           'distance from Vasaplatsen to Doktor Sydows Gata': 2.223}
 
-#-----------------------------
-#lines_via_stop
-try:
-    assert answer_query(tramdict, 'via Jaegerdorffsplatsen') == ['3', '9']
-except:
-    print(f"Error in lines_via_stop via Jaegerdorffsplatsen, your result is {answer_query(tramdict, 'via Jaegerdorffsplatsen')}, but the expected is {['3', '9']}")
-    outcome = False
-
-try:
-    assert answer_query(tramdict, 'via Aprilgatan') == ['6']
-except:
-    print(f"Error in lines_via_stop via Aprilgatan, your result is {answer_query(tramdict, 'via Aprilgatan')}, but the expected is {['6']}")
-    outcome = False
-
-try:
-    assert answer_query(tramdict, 'via Scandinavium') == ['2', '6', '8', '13']
-except:
-    print(f"Error in lines_via_stop via Scandinavium, your result is {answer_query(tramdict, 'via Scandinavium')}, but the expected is {['2', '6', '8', '13']}")
-    outcome = False
-
-try:    
-    assert answer_query(tramdict, 'via Botaniska Trädgården') == ['1', '2', '7', '8', '13']
-except:
-    print(f"Error in lines_via_stop via Botaniska Trädgården, your result is {answer_query(tramdict, 'via Botaniska Trädgården')}, but the expected is {['1', '2', '7', '8', '13']}")
-    outcome = False
-
-try:
-    assert answer_query(tramdict, 'via Chalmers') == ['6', '7', '8', '10', '13']
-except:
-    print(f"Error in lines_via_stop via Chalmers, your result is {answer_query(tramdict, 'via Chalmers')}, but the expected is {['6', '7', '8', '10', '13']}")
-    outcome = False
-
-#-----------------------------
-#lines_between_stops
-try:
-    assert answer_query(tramdict, 'between Temperaturgatan and Sahlgrenska Huvudentré') == ['6']
-except:
-    print(f"Error in lines_between_stops between Temperaturgatan and Sahlgrenska Huvudentré, your result is {answer_query(tramdict, 'between Temperaturgatan and Sahlgrenska Huvudentré')}, but the expected is {['6']}")
-    outcome = False
-
-try:    
-    assert answer_query(tramdict, 'between Domkyrkan and Scandinavium') == ['2', '6']
-except:
-    print(f"Error in lines_between_stops between Domkyrkan and Scandinavium, your result is {answer_query(tramdict, 'between Domkyrkan and Scandinavium')}, but the expected is {['2', '6']}")
-    outcome = False
-
-try:
-    assert answer_query(tramdict, 'between Bellevue and Positivgatan') == ['7']
-except:
-    print(f"Error in lines_between_stops between Bellevue and Positivgatan, your result is {answer_query(tramdict, 'between Bellevue and Positivgatan')}, but the expected is {['7']}")
-    outcome = False
-
-try:
-    assert answer_query(tramdict, 'between Saltholmen and Medicinaregatan') == ['13']
-except:
-    print(f"Error in lines_between_stops between Saltholmen and Medicinaregatan, your result is {answer_query(tramdict, 'between Saltholmen and Medicinaregatan')}, but the expected is {['13']}")
-    outcome = False
-
-try:
-    assert answer_query(tramdict, 'between Saltholmen and Medicinaregatan') == answer_query(tramdict, 'between Medicinaregatan and Saltholmen')
-except:
-    print(f"Error in lines_between_stops between Saltholmen and Medicinaregatan, your result is {answer_query(tramdict, 'between Saltholmen and Medicinaregatan')}, but the expected is {answer_query(tramdict, 'between Medicinaregatan and Saltholmen')}")
-    outcome = False
-
-#-----------------------------
-#time_between_stops
-try:
-    assert answer_query(tramdict, 'time with 3 from Hagakyrkan to Mariaplan') == 16
-except:
-    print(f"Error in time_between_stops time with 3 from Hagakyrkan to Mariaplan, your result is {answer_query(tramdict, 'time with 3 from Hagakyrkan to Mariaplan')}, but the expected is {16}")
-    outcome = False
-
-try:
-    assert answer_query(tramdict, 'time with 5 from Munkebäckstorget to Sankt Sigfrids Plan') == 9
-except:
-    print(f"Error in time_between_stops time with 5 from Munkebäckstorget to Sankt Sigfrids Plan, your result is {answer_query(tramdict, 'time with 5 from Munkebäckstorget to Sankt Sigfrids Plan')}, but the expected is {9}")
-    outcome = False
-
-try:
-    assert answer_query(tramdict, 'time with 7 from Allhelgonakyrkan to Komettorget') == answer_query(tramdict, 'time with 7 from Komettorget to Allhelgonakyrkan') 
-except:
-    print(f"Error in time_between_stops time with 7 from Allhelgonakyrkan to Komettorget, your result is {answer_query(tramdict, 'time with 7 from Allhelgonakyrkan to Komettorget')}, but the expected is {answer_query(tramdict, 'time with 7 from Komettorget to Allhelgonakyrkan')}")
-    outcome = False
-
-try:
-    assert answer_query(tramdict, 'time with 10 from Wavrinskys Plats to Hjalmar Brantingsplatsen') == 15
-except:
-    print(f"Error in time_between_stops time with 10 from Wavrinskys Plats to Hjalmar Brantingsplatsen, your result is {answer_query(tramdict, 'time with 10 from Wavrinskys Plats to Hjalmar Brantingsplatsen')}, but the expected is {15}")
-    outcome = False
-
-try:
-    assert answer_query(tramdict, 'time with 13 from Scandinavium to Korsvägen') == 1
-except:
-    print(f"Error in time_between_stops time with 13 from Scandinavium to Korsvägen, your result is {answer_query(tramdict, 'time with 13 from Scandinavium to Korsvägen')}, but the expected is {1}")
-    outcome = False
-
-#-----------------------------
-#distance_between_stops
-try:
-    assert round(answer_query(tramdict, 'distance from Jaegerdorffsplatsen to Tranered'), 3) == 3.003
-except:
-    print(f"Error in distance_between_stops distance from Jaegerdorffsplatsen to Tranered, your result is {answer_query(tramdict, 'distance from Jaegerdorffsplatsen to Tranered')}, but the expected is {3.003}")
-    outcome = False
-
-try:
-    assert round(answer_query(tramdict, 'distance from Sahlgrenska Huvudentré to Kortedala Torg'), 3) == round(answer_query(tramdict, 'distance from Kortedala Torg to Sahlgrenska Huvudentré'), 3)
-except:
-    print(f"Error in distance_between_stops distance from Sahlgrenska Huvudentré to Kortedala Torg, your result is {answer_query(tramdict, 'distance from Sahlgrenska Huvudentré to Kortedala Torg')}, but the expected is {answer_query(tramdict, 'distance from Kortedala Torg to Sahlgrenska Huvudentré')}")
-    outcome = False
-
-try:
-    assert round(answer_query(tramdict, 'distance from Temperaturgatan to Lackarebäck'), 3) == 10.092
-except:
-    print(f"Error in distance_between_stops distance from Temperaturgatan to Lackarebäck, your result is {answer_query(tramdict, 'distance from Temperaturgatan to Lackarebäck')}, but the expected is {10.092}")
-    outcome = False
-
-try:
-    assert round(answer_query(tramdict, 'distance from Rymdtorget Spårvagn to Hagakyrkan'), 3) == 8.841
-except:
-    print(f"Error in distance_between_stops distance from Rymdtorget Spårvagn to Hagakyrkan, your result is {answer_query(tramdict, 'distance from Rymdtorget Spårvagn to Hagakyrkan')}, but the expected is {8.841}")
-    outcome = False
-
-try:
-    assert round(answer_query(tramdict, 'distance from Vasaplatsen to Doktor Sydows Gata'), 3) == 2.223
-except:
-    print(f"Error in distance_between_stops distance from Vasaplatsen to Doktor Sydows Gata, your result is {answer_query(tramdict, 'distance from Vasaplatsen to Doktor Sydows Gata')}, but the expected is {2.223}")
-    outcome = False
-
+for query, answer in answers.items():
+    if type(answer) == float:
+        try:
+            assert round(answer_query(tramdict, query), 3) == answer
+        except:
+            print(f"Error in {query}, your result is {round(answer_query(tramdict, query), 3)}, but the expected is {answer}")
+            outcome = False
+    else:
+        try:
+            assert answer_query(tramdict, query) == answer
+        except:
+            print(f"Error in {query}, your result is {answer_query(tramdict, query)}, but the expected is {answer}")
+            outcome = False
 
 #-----------------------------
 #final result
